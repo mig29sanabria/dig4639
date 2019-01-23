8bcc7c99f63c003f5579f290f1b3810188cffb75
mi29 <36017382+mi29@users.noreply.github.com>
2019-01-23 16:36:04 -0500

    Added a greeting using React

---
+++ lab1/src/App.js
@@ -0,0 +1,14 @@
+import NameForm from './NameForm';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+      return (
+          <NameForm />
+      );
+  }
+}
+
+export default App;
